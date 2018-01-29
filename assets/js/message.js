2d0a0aa265eac3cd0c1e5826aa54dee5780753d9
mayufo <mayufo@aliyun.com>
2018-01-30 00:30:31 +0800

    completed

--- assets/js/message.js
+++ assets/js/message.js
@@ -5,6 +5,71 @@
 !function () {
     // var view = document.querySelector('#
     var view = document.querySelector('#message')
+
+    var controller = Controller({
+        init: function (view, controller) {
+            this.messageForm = view.querySelector('form')
+            this.messageList = view.querySelector('ul')
+            this.loadMessages()            
+        },
+        loadMessages: () => {
+            // 获取
+            var query = new AV.Query('Message');
+            query.find().then((message) => {
+                let array = message.map((item) => item.attributes)
+                array.forEach(item => {
+                    let li = document.createElement('li')
+                    let h3 = document.createElement('h3')
+                    console.log(item.name)
+                    h3.innerText = `${item.name}:`
+                    let p = document.createElement('p')
+                    p.innerText = `${item.message}`
+                    li.appendChild(h3)
+                    li.appendChild(p)
+                    this.messageList.appendChild(li)
+                })
+            }, (error) => {
+                // 异常处理
+                alert('请稍后留言')
+            });
+        },
+        save: () => {
+            let name = this.messageForm.querySelector('input[name=name]').value
+            let message = this.messageForm.querySelector('input[name=content]').value
+            if (!name) {
+                alert('请输入用户名')
+            } else if (!message) {
+                alert('请输入留言内容')
+            } else if (name && message) {
+                const Message = AV.Object.extend('Message');
+                const messageObject = new Message();
+                messageObject.save({
+                    name,
+                    message
+                }).then((res) => {
+                    res = res.attributes
+                    alert('发送成功')
+                    this.messageForm.querySelector('input[name=name]').value = ''
+                    this.messageForm.querySelector('input[name=content]').value = ''
+                    let li = document.createElement('li')
+                    let h3 = document.createElement('h3')
+                    h3.innerText = `${res.name}:`
+                    let p = document.createElement('p')
+                    p.innerText = `${res.message}`
+                    li.appendChild(h3)
+                    li.appendChild(p)
+                    this.messageList.appendChild(li)
+                })
+            }
+        },
+        bindEvents: function() {
+            // 存储
+            this.messageForm.addEventListener('submit', (e) => {
+                e.preventDefault()
+                this.save()
+            })
+        },
+    })
     var controller = {
         view: null,
         init: function(view) {
@@ -43,7 +108,6 @@
                 // 异常处理
                 alert('请稍后留言')
             });
-
         },
         bindEvents: function() {
             // 存储
